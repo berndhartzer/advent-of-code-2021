a6847bd7b54a77b718220221961e8b1e06af22d2
Bernd Hartzer <berndhartzer@gmail.com>
2021-12-11 13:16:24 +1000

    day 10 pt 2

--- 10_syntax_scoring_test.go
+++ 10_syntax_scoring_test.go
@@ -2,6 +2,7 @@ package aoc
 
 import (
 	"fmt"
+	"sort"
 	"testing"
 	"time"
 )
@@ -16,6 +17,19 @@ const (
 	openAngle   = rune('<')
 	closeAngle  = rune('>')
 )
+
+var closer = map[rune]rune{
+	closeParens: openParens,
+	closeBrace:  openBrace,
+	closeSquare: openSquare,
+	closeAngle:  openAngle,
+}
+var opener = map[rune]rune{
+	openParens: closeParens,
+	openBrace:  closeBrace,
+	openSquare: closeSquare,
+	openAngle:  closeAngle,
+}
 
 type syntaxStack struct {
 	values []rune
@@ -37,14 +51,6 @@ func (s *syntaxStack) length() int {
 }
 
 func syntaxScoringPartOne(lines []string) int {
-	stack := syntaxStack{}
-
-	closer := map[rune]rune{
-		closeParens: openParens,
-		closeBrace:  openBrace,
-		closeSquare: openSquare,
-		closeAngle:  openAngle,
-	}
 	scorer := map[rune]int{
 		closeParens: 3,
 		closeBrace:  1197,
@@ -55,6 +61,8 @@ func syntaxScoringPartOne(lines []string) int {
 	score := 0
 
 	for _, line := range lines {
+		stack := syntaxStack{}
+
 		for _, c := range line {
 			switch c {
 			case openParens:
@@ -83,6 +91,59 @@ func syntaxScoringPartOne(lines []string) int {
 	}
 
 	return score
+}
+
+func syntaxScoringPartTwo(lines []string) int {
+	scorer := map[rune]int{
+		openParens: 1,
+		openBrace:  3,
+		openSquare: 2,
+		openAngle:  4,
+	}
+
+	lineScores := []int{}
+
+lineLooper:
+	for _, line := range lines {
+		stack := syntaxStack{}
+
+		for _, c := range line {
+			switch c {
+			case openParens:
+				fallthrough
+			case openBrace:
+				fallthrough
+			case openSquare:
+				fallthrough
+			case openAngle:
+				stack.push(c)
+			case closeParens:
+				fallthrough
+			case closeBrace:
+				fallthrough
+			case closeSquare:
+				fallthrough
+			case closeAngle:
+				head := stack.pop()
+				expected := closer[c]
+				if head != expected {
+					continue lineLooper
+				}
+			}
+		}
+
+		lineScore := 0
+		for stack.length() > 0 {
+			lineScore *= 5
+			lastOpen := stack.pop()
+			lineScore += scorer[lastOpen]
+		}
+
+		lineScores = append(lineScores, lineScore)
+	}
+
+	sort.Ints(lineScores)
+	return lineScores[(len(lineScores) / 2)]
 }
 
 func TestDayTen(t *testing.T) {
@@ -140,5 +201,31 @@ func TestDayTen(t *testing.T) {
 		}
 
 		runTests(t, tests, syntaxScoringPartOne)
+	})
+
+	t.Run("part two", func(t *testing.T) {
+		tests := map[string]testConfig{
+			"test 1": {
+				input: []string{
+					"[({(<(())[]>[[{[]{<()<>>",
+					"[(()[<>])]({[<{<<[]>>(",
+					"{([(<{}[<>[]}>{[]{[(<()>",
+					"(((({<>}<{<{<>}{[]{[]{}",
+					"[[<[([]))<([[{}[[()]]]",
+					"[{[{({}]{}}([{[{{{}}([]",
+					"{<[[]]>}<{[{[{[]{()[[[]",
+					"[<(<(<(<{}))><([]([]()",
+					"<{([([[(<>()){}]>(<<{{",
+					"<{([{{}}[<[[[<>{}]]]>[]]",
+				},
+				expected: 288957,
+			},
+			"solution": {
+				input:     input,
+				logResult: true,
+			},
+		}
+
+		runTests(t, tests, syntaxScoringPartTwo)
 	})
 }
